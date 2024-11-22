aec6ffea2ec003baa6874968a613d17dfa21ed9e
Eduard Tomek <eduard.tomek@usu.com>
2024-11-22 10:32:22 +0100

    Spring initializr commit

---
+++ src/main/kotlin/com/example/flowable_cds/FlowableCdsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flowable_cds
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowableCdsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowableCdsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/flowable_cds/FlowableCdsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flowable_cds
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowableCdsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
